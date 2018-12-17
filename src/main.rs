a3d1d5bb02b24cde13197da5f217c2a8b5966fe5
Thomas Hurst <tom@hur.st>
2018-12-17 16:14:28 +0000

    Take repo pack as an arg in example

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 extern crate svnlook;
 use svnlook::SvnRepo;
 
+use std::env;
+
 fn main() {
-    let repo = SvnRepo::new("/repos/freebsd");
+    let repo = SvnRepo::new(env::args_os().nth(1).expect("Need a path"));
 
-    let latest = repo.youngest().expect("latest");
+    let latest = repo.youngest().expect("youngest");
 
-    for rev in 1000..latest {
+    for rev in 1..latest {
         let info = repo.info(rev).expect("info");
         let changed = repo.changed(rev).expect("changed");
 
