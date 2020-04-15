e3485aa140251aebe6da887833c4f9c0effe8d5c
Thomas Hurst <tom@hur.st>
2020-04-15 10:45:38 +0000

    Make example more extensive

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,58 @@ use svnlook::{SvnRepo, SvnStatus};
 use std::env;
 
 fn main() -> Result<(), svnlook::SvnError> {
-    let repo = SvnRepo::new(env::args_os().nth(1).expect("Need a path"));
+    let cmd = env::args().nth(1).expect("Need a command");
+    let repo = SvnRepo::from(env::args_os().nth(2).expect("Need a repository path"));
 
-    let latest = repo.youngest()?;
-
-    for rev in 1..latest {
-        let info = repo.info(rev)?;
-        let changed = repo.changed(rev)?;
-
-        println!(
-            "Revision r{}, by {} at {}",
-            info.revision, info.committer, info.date
-        );
-        for change in changed {
-            let change = change?;
-            print!("   {:.8}: ", change.status);
-
-            if let SvnStatus::Copied(Some(ref from)) = change.status {
-                print!("{}@r{} -> ", from.path.display(), from.revision);
+    match &cmd[..] {
+        "youngest" => println!("{}", repo.youngest()?),
+        "changes" => {
+            let rev: u32 = env::args().nth(3).expect("Need a revision").parse().expect("Not a number");
+            for change in repo.changed(rev)? {
+                let change = change?;
+                print!("   {:.8}: ", change.status);
+    
+                if let SvnStatus::Copied(Some(ref from)) = change.status {
+                    print!("{}@r{} -> ", from.path.display(), from.revision);
+                }
+    
+                println!("{}", change.path.display());
             }
+        },
+        "diff" => {
+            let rev: u32 = env::args().nth(3).expect("Need a revision").parse().expect("Not a number");
+            std::io::copy(&mut repo.diff(rev)?, &mut std::io::stdout())?;
+        }
+        "cat" => {
+            let rev: u32 = env::args().nth(3).expect("Need a revision").parse().expect("Not a number");
+            let path = env::args().nth(4).expect("Need a file path");
+            std::io::copy(&mut repo.cat(rev, path)?, &mut std::io::stdout())?;
+        }
+        "walk" => {
+            let latest = repo.youngest()?;
 
-            println!("{}", change.path.display());
+            for rev in 1..latest {
+                let info = repo.info(rev)?;
+                let changed = repo.changed(rev)?;
+        
+                println!(
+                    "Revision r{}, by {} at {}",
+                    info.revision, info.committer, info.date
+                );
+                for change in changed {
+                    let change = change?;
+                    print!("   {:.8}: ", change.status);
+        
+                    if let SvnStatus::Copied(Some(from)) = change.status {
+                        print!("{}@r{} -> ", from.path.display(), from.revision);
+                    }
+        
+                    println!("{}", change.path.display());
+                }
+            }
+        }
+        _ => {
+            panic!("Commands: youngest, changes, diff, cat, walk");
         }
     }
 
